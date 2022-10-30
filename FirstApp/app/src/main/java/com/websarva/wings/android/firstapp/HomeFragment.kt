feb686c68d0b44be199df17da616eacdf3e8fe0c
DESKTOP-H3EKBHC\tkent <t.kent.work@gmail.com>
2022-10-30 11:22:38 +0900

    add home product list

--- FirstApp/app/src/main/java/com/websarva/wings/android/firstapp/HomeFragment.kt
+++ FirstApp/app/src/main/java/com/websarva/wings/android/firstapp/HomeFragment.kt
@@ -5,8 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.websarva.wings.android.firstapp.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -14,5 +17,27 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+
+        when(binding){
+
+        }
+
+        binding.item1.setOnClickListener {  }
+        binding.item2.setOnClickListener {  }
+        binding.item3.setOnClickListener {  }
+        binding.item4.setOnClickListener {  }
+        binding.item5.setOnClickListener {  }
+        binding.item6.setOnClickListener {  }
+        binding.item7.setOnClickListener {  }
+        binding.item8.setOnClickListener {  }
+        binding.item9.setOnClickListener {  }
+        binding.item10.setOnClickListener {  }
+        binding.item11.setOnClickListener {  }
+        binding.item12.setOnClickListener {  }
     }
 }
